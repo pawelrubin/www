8b1332a85130078309520e81a49611e165978a13
Paweł Rubin <pawelrubin19@gmail.com>
2020-01-03 15:31:25 +0100

    email in registration

--- l5/registration.php
+++ l5/registration.php
@@ -3,12 +3,14 @@
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
   $username = $_POST["username"];
   $password = hash("sha256", $_POST["password"]);
+  $email = $_POST["email"];
 
   $db = new SQLite3("database.db");
 
-  $stmt = $db->prepare("INSERT INTO USERS VALUES (:usr, :psw)");
+  $stmt = $db->prepare("INSERT INTO USERS VALUES (:usr, :psw, :eml)");
   $stmt->bindValue(":usr", $username, SQLITE3_TEXT);
   $stmt->bindValue(":psw", $password, SQLITE3_TEXT);
+  $stmt->bindValue(":eml", $email, SQLITE3_TEXT);
   
   if ($stmt->execute()) {
     setcookie("username", $username, time() + 60*5);
@@ -24,6 +26,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
   <div class="input-group">
     <label>Password</label>
     <input type="password" name="password">
+  </div>
+  <div class="input-group">
+    <label>E-mail</label>
+    <input type="email" name="email">
   </div>
   <div class="input-group">
     <button type="submit" class="btn">Register</button>
